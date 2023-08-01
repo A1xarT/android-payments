197356991507e93a28817ec3e73376c92486d8c7
A1xarT <illya030778@gmail.com>
2023-08-02 01:45:28 +0300

    added simple interface (revolut only)

--- app/src/main/java/com/example/AndroidPaymentApp/MainActivity.java
+++ app/src/main/java/com/example/AndroidPaymentApp/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.stripepayments;
+package com.example.AndroidPaymentApp;
 
 import static com.revolut.revolutpay.api.RevolutPayKt.createController;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,6 +15,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -31,6 +35,8 @@ import com.stripe.android.paymentsheet.PaymentSheetResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     PaymentSheet paymentSheet;
     String paymentIntentClientSecret;
     String revolutOrderId;
+    boolean isLoadingRevolutApi;
     PaymentSheet.CustomerConfiguration configuration;
 
     @Override
@@ -47,30 +54,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setStripeButton();
         setRevolutButton();
-        setHandpointButton();
+//        setStripeButton();
+//        setHandpointButton();
     }
 
-    private void setHandpointButton() {
-        Button handpointButton = findViewById(R.id.pay_hp);
-
-    }
-
-    private void setStripeButton() {
-        Button stripeButton = findViewById(R.id.pay_stripe);
-        paymentSheet = new PaymentSheet(this, this::onPaymentSheetResult);
-        stripeButton.setOnClickListener(v -> {
-            fetchStripeApi();
-            if (paymentIntentClientSecret != null) {
-                paymentSheet.presentWithPaymentIntent(paymentIntentClientSecret,
-                        new PaymentSheet.Configuration("Merchant Name 123", configuration));
-            } else
-                Toast.makeText(getApplicationContext(), "API Loading ...", Toast.LENGTH_SHORT).show();
-        });
-    }
+//    private void setHandpointButton() {
+//        Button handpointButton = findViewById(R.id.pay_hp);
+//
+//    }
+//
+//    private void setStripeButton() {
+//        Button stripeButton = findViewById(R.id.pay_stripe);
+//        paymentSheet = new PaymentSheet(this, this::onPaymentSheetResult);
+//        stripeButton.setOnClickListener(v -> {
+//            fetchStripeApi();
+//            if (paymentIntentClientSecret != null) {
+//                paymentSheet.presentWithPaymentIntent(paymentIntentClientSecret,
+//                        new PaymentSheet.Configuration("Merchant Name 123", configuration));
+//            } else
+//                Toast.makeText(getApplicationContext(), "API Loading ...", Toast.LENGTH_SHORT).show();
+//        });
+//    }
 
     private void setRevolutButton() {
+        isLoadingRevolutApi = false;
+        revolutOrderId = null;
         RevolutPayButton revolutButton = findViewById(R.id.revolut_pay_button);
         RevolutPay revolutPay = RevolutPayExtensionsKt.getRevolutPay(RevolutPayments.INSTANCE);
         Uri returnUri = new Uri.Builder().scheme("scheme1").authority("host").build();
@@ -82,8 +91,19 @@ public class MainActivity extends AppCompatActivity {
                 flow.attachLifecycle(this.getLifecycle());
                 flow.continueConfirmationFlow();
             } else {
-                fetchRevolutApi();
-                Toast.makeText(getApplicationContext(), "API Loading ...", Toast.LENGTH_SHORT).show();
+                if (!isLoadingRevolutApi) {
+                    isLoadingRevolutApi = true;
+                    fetchRevolutApi(orderId -> {
+                        isLoadingRevolutApi = false;
+                        if (orderId != null) {
+                            revolutOrderId = orderId;
+                            flow.setOrderToken(revolutOrderId);
+                            flow.attachLifecycle(getLifecycle());
+                            flow.continueConfirmationFlow();
+                        }
+                    });
+                    Toast.makeText(getApplicationContext(), "Loading...", Toast.LENGTH_SHORT).show();
+                }
             }
             return null;
         });
@@ -98,31 +118,77 @@ public class MainActivity extends AppCompatActivity {
                 revolutOrderId = null;
             }
         });
+
+        // Find the EditText fields and add TextWatcher listeners
+        EditText amountEditText = findViewById(R.id.amount);
+        EditText descriptionEditText = findViewById(R.id.description);
+        amountEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Not used, but required to implement
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                revolutOrderId = null; // Set revolutOrderId to null when the amount changes
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Not used, but required to implement
+            }
+        });
+
+        descriptionEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Not used, but required to implement
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                revolutOrderId = null; // Set revolutOrderId to null when the description changes
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Not used, but required to implement
+            }
+        });
+
+
     }
 
-    private void fetchRevolutApi() {
+    private interface ApiLoadCallback {
+        void onApiLoaded(String revolutOrderId);
+    }
+
+    private void fetchRevolutApi(ApiLoadCallback callback) {
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.getCache().clear();
         String url = BuildConfig.REVOLUT_API_URL;
-        // Prepare the request data as a JSONObject
-        int amount = Integer.parseInt(((EditText) findViewById(R.id.amount)).getText().toString());
+        // Prepare the request data as key-value pairs
+        int amount = Integer.parseInt(((EditText) findViewById(R.id.amount)).getText().toString().replaceAll("[,.]", ""));
         String description = ((EditText) findViewById(R.id.description)).getText().toString();
-        String currency = ((Spinner) findViewById(R.id.currencySpinner)).getSelectedItem().toString();
+        String currency = "USD";
 
-        JSONObject requestData = new JSONObject();
+        String requestData = null;
         try {
-            requestData.put("amount", amount);
-            requestData.put("currency", currency);
-            requestData.put("description", description);
-        } catch (JSONException exception) {
-            exception.printStackTrace();
+            requestData = "amount=" + amount + "&currency=" + currency + "&description=" + URLEncoder.encode(description, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
         }
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, requestData,
+
+        String finalRequestData = requestData;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                 response -> {
                     try {
-                        revolutOrderId = response.getString("public_id");
+                        JSONObject jsonObject = new JSONObject(response);
+                        revolutOrderId = jsonObject.getString("public_id");
+                        callback.onApiLoaded(revolutOrderId); // Notify the callback with the orderId
                     } catch (JSONException e) {
                         e.printStackTrace();
+                        Log.d("Err", response);
                         Toast.makeText(this, "Json Exception", Toast.LENGTH_SHORT).show();
                     }
                 },
@@ -133,15 +199,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public Map<String, String> getHeaders() {
                 Map<String, String> headers = new HashMap<>();
-                headers.put("Content-Type", "application/json");
+                headers.put("Content-Type", "application/x-www-form-urlencoded");
                 headers.put("AUTH", BuildConfig.REVOLUT_MERCHANT_API_KEY);
                 return headers;
             }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return finalRequestData.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
         };
-        jsonObjectRequest.setShouldCache(false);
-        queue.add(jsonObjectRequest);
+        stringRequest.setShouldCache(false);
+        queue.add(stringRequest);
         queue.getCache().remove(url);
     }
+
 
 
     private void onPaymentSheetResult(final PaymentSheetResult paymentSheetResult) {
