b3346e22250a75daeb6ba34bcd77752d39b80f9a
A1xarT <illya030778@gmail.com>
2023-09-19 21:17:27 +0300

    added updater

--- app/src/main/java/com/example/AndroidPaymentApp/StartScreenActivity.java
+++ app/src/main/java/com/example/AndroidPaymentApp/StartScreenActivity.java
@@ -6,12 +6,16 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+
 public class StartScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        startUpdatesCheckService();
         ImageView createPaymentButton = findViewById(R.id.create_payment);
         createPaymentButton.setOnClickListener(v -> {
             Intent intent = new Intent(StartScreenActivity.this, MainActivity.class);
@@ -22,5 +26,14 @@ public class StartScreenActivity extends AppCompatActivity {
             Intent intent = new Intent(StartScreenActivity.this, MerchantKeyScreenActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void startUpdatesCheckService() {
+        AppUpdater appUpdater = new AppUpdater(this)
+                .setUpdateFrom(UpdateFrom.JSON)
+                .setUpdateJSON("https://raw.githubusercontent.com/A1xarT/android-payments/main/update-changelog.json")
+                .setButtonDoNotShowAgain(null)
+                .setCancelable(false);
+        appUpdater.start();
     }
 }
